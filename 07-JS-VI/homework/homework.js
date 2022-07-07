63c8c1fed814ba9dc8bf6b2d73d7bab792566d77
Apple <apple@MacBook-Pro-de-Apple.local>
2022-07-07 09:50:28 -0300

    second commit

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -45,11 +45,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  var arr = []
-  array.map(function(x){
-    cb(x)
-    arr.push(x)
+  var arr = array.map(function(x){
+    return cb(x)
   })
+  return arr
 }
 
 function filter(array) {
